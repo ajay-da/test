28e5ed64d87ae941f18a4f0b678ff81c8b559841
ajay-da <96560983+ajay-da@users.noreply.github.com>
2021-12-27 09:26:49 +0530

    Add files via upload

---
+++ Assessment1.js
@@ -0,0 +1,78 @@
+const quiz=[
+    {
+    q:'Which month comes right before june?',
+    options:['may','july','august','december'],
+    answer:0
+},
+{
+    q:'what is the spelling of "True"?',
+    options:['True','Ture','ruTe','eTur'],
+    answer:0
+},
+
+{
+    q:'How many days are there in a year?',
+    options:['365','475','65','1059'],
+    answer:0
+},
+{
+    q:'Verdict of Spider-man no way home ',
+    options:['Blockbuster','Hit','Superhit','flop'],
+    answer:0
+},
+{
+    q:'Have I cleared the assessment',
+    options:['yes','no'],
+    answer:0
+},
+{
+    q:'Which is the worst WIFI password?',
+    options:['123456','abcdef','010101','password'],
+    answer:0
+},
+{
+    q:'2+2 ?',
+    options:['4','22','42','8'],
+    answer:0
+},
+{
+    q:'For how many years did the hundred years war last?',
+    options:['100','50','25','125'],
+    answer:0
+},
+{
+    q:'Which country makes panama hats',
+    options:['panama','pujara','china','india'],
+    answer:0
+},
+{
+    q:'What was Rajini sir last movie',
+    options:['Annaatthe','Darbar','Valimai','Pettai'],
+    answer:0
+},
+{
+    q:'Does Kailasa really exist?',
+    options:['Yes','No'],
+    answer:0
+},
+{
+    q:'7*7?',
+    options:['49','77','89','23'],
+    answer:0
+},
+{
+    q:'Who killed thanos in Avenger EndGame?',
+    options:['IronMan','Captian America','Thor','Loki'],
+    answer:0
+},
+{
+    q:'Who Killed Bahubali?',
+    options:['Kattappa','Rajamatha','Devasenai'],
+    answer:0
+},
+{
+    q:'VPN stands for?',
+    options:['Virtual Private Network','Vishal Private Network','Verappan Public Network','Visual Private Network'],
+    answer:0
+}
+]
